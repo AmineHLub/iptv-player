c60a334ccdf0a5fc12c9312096d481cea3ac2ddf
AmineHLub <Amino.bhl@gmail.com>
2022-07-29 15:32:36 +0100

    Static splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import iptvLogo from './Assets/iptv-logo.png'
+import m3uLogo from './Assets/m3u-logo.png'
+import xtreamLogo from './Assets/xtream-logo.png'
 import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="fixed-splash">
+        <img src={iptvLogo} alt='splash-logo' />
+        <div className="selection-container">
+          <div className="selection upload-container">
+            <img src={m3uLogo} alt='' />
+            <button>Upload M3U</button>
+          </div>
+          <div className="selection xtream-container">
+          <img src={xtreamLogo} alt='' />
+            <button>XTREAM</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
